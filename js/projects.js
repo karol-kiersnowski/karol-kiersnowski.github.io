7cdff562aa38ee88f1266cea9c17cf6e846b24cd
Karol <karol.kiersnowski@outlook.com>
2021-02-20 14:51:21 +0100

    change the project table

--- js/projects.js
+++ js/projects.js
@@ -1,25 +1,19 @@
-function setTableView() {
-	document.getElementById("project-select").style.display = "none";
-	document.getElementById("project-grid-list").style.display = "none";
-	document.getElementById("project-table").style.display = "table";
-}
+var projectSelect = document.getElementById("project-select");
+var projectContainer = document.getElementsByClassName("project-container")[0];
 
-function setGridView() {
-	document.getElementById("project-table").style.display = "none";
-	document.getElementById("project-select").style.display = "inline-block";
-	document.getElementById("project-grid-list").style.display = "block";
-	var list = document.getElementById("project-list");
-	if (list != null)
-		document.getElementById("project-list").id = "project-grid";
+function setTableView() {
+	projectSelect.style.display = "none";
+	projectContainer.id = "project-table";
 }
 
 function setListView() {
-	document.getElementById("project-table").style.display = "none";
-	document.getElementById("project-select").style.display = "inline-block";
-	document.getElementById("project-grid-list").style.display = "block";
-	var grid = document.getElementById("project-grid");
-	if (grid != null)
-		document.getElementById("project-grid").id = "project-list";
+	projectSelect.style.display = "inline-block";
+	projectContainer.id = "project-list";
+}
+
+function setGridView() {
+	projectSelect.style.display = "inline-block";
+	projectContainer.id = "project-grid";
 }
 
 function toggleTableColumn(checkbox, column) {
@@ -38,7 +32,7 @@ function toggleTableColumn(checkbox, column) {
 
 function sortTable(n, thisElement) {
 	var table, rows, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
-	table = document.getElementById("project-table");
+	table = document.getElementsByClassName("project-container")[0];
 	switching = true;
 	direction = "asc"; 
 	/*Make a loop that will continue until
@@ -89,79 +83,10 @@ function sortTable(n, thisElement) {
 		}
 	}
 
-	translateText(document.documentElement.lang);
-	if (direction == "asc") thisElement.innerHTML += ' <i class="fa fa-sort-down"></i>';
-	else if (direction == "desc") thisElement.innerHTML += ' <i class="fa fa-sort-up"></i>';
-}
-
-function sort(sortingBy) {
-	var elements, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
-	switching = true;
-	//Set the sorting direction to ascending:
-	direction = "asc"; 
-	/*Make a loop that will continue until
-	no switching has been done:*/
-	while (switching) {
-		//start by saying: no switching is done:
-		switching = false;
-		elements = document.getElementsByClassName("item");
-		/*Loop through all table elements (except the
-		first, which contains table headers):*/
-		for (i = 0; i < (elements.length - 1); i++) {
-			//start by saying there should be no switching:
-			shouldSwitch = false;
-			/*Get the two elements you want to compare,
-			one from current row and one from the next:*/
-			//alert(elements[i].children[1].innerHTML);
-			if (sortingBy == "name") {
-				console.log(elements[i].children[1]);
-				x = elements[i].children[1];
-				y = elements[i + 1].children[1];
-			} else if (sortingBy == "update") {
-				x = elements[i].children[2].children[1];
-				y = elements[i + 1].children[2].children[1];
-			} else if (sortingBy == "initiate") {
-				x = elements[i].children[3].children[1];
-				y = elements[i + 1].children[3].children[1];
-			} else if (sortingBy == "progress") {
-				console.log(elements[i].children[6]);
-				x = elements[i].children[6];
-				y = elements[i + 1].children[6];
-			} else if (sortingBy == "language") {
-				x = elements[i].children[8].children[1];
-				y = elements[i + 1].children[8].children[1];
-			}
-			/*check if the two elements should switch place,
-			based on the direction, asc or desc:*/
-			if (direction == "asc") {
-				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-					//if so, mark as a switch and break the loop:
-					shouldSwitch= true;
-					break;
-				}
-			} else if (direction == "desc") {
-				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-					//if so, mark as a switch and break the loop:
-					shouldSwitch = true;
-					break;
-				}
-			}
-		}
-		if (shouldSwitch) {
-			/*If a switch has been marked, make the switch
-			and mark that a switch has been done:*/
-			elements[i].parentNode.insertBefore(elements[i + 1], elements[i]);
-			switching = true;
-			//Each time a switch is done, increase this count by 1:
-			switchcount ++;      
-		} else {
-			/*If no switching has been done AND the direction is "asc",
-			set the direction to "desc" and run the while loop again.*/
-			if (switchcount == 0 && direction == "asc") {
-				direction = "desc";
-				switching = true;
-			}
-		}
+	if (thisElement != null) {
+		translateText(document.documentElement.lang);
+		if (direction == "asc") thisElement.innerHTML += ' <i class="fa fa-sort-down"></i>';
+		else if (direction == "desc") thisElement.innerHTML += ' <i class="fa fa-sort-up"></i>';
 	}
 }
 
